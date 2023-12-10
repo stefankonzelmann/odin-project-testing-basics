c7fb998c9788f317acf826488c8743b6d2cf2ded
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-12-10 21:26:39 +0100

    Implement Calculator

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function calculator() {
+  calc = {
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+  };
+  return calc;
+}
+module.exports = calculator;
--- jest.test.js
+++ jest.test.js
@@ -1,5 +1,6 @@
 const capitalize = require("./capizalize");
 const reverseString = require("./reverseString");
+const calculator = require("./calculator");
 
 test("capitalize", () => {
   expect(capitalize("stefan")).toBe("Stefan");
@@ -7,4 +8,12 @@ test("capitalize", () => {
 
 test("reverseString", () => {
   expect(reverseString("stefan")).toBe("nafets");
+});
+
+test("calculator", () => {
+  calc = calculator();
+  expect(calc.add(2, 5)).toBe(7);
+  expect(calc.subtract(2, 5)).toBe(-3);
+  expect(calc.multiply(2, 5)).toBe(10);
+  expect(calc.divide(2, 5)).toBe(0.4);
 });
