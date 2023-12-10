aa9124ae5f0c9adeac75059c430bc9fa0cd8de4e
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-12-10 21:17:32 +0100

    Implement reverseString

--- jest.test.js
+++ jest.test.js
@@ -1,5 +1,10 @@
 const capitalize = require("./capizalize");
+const reverseString = require("./reverseString");
 
 test("capitalize", () => {
   expect(capitalize("stefan")).toBe("Stefan");
+});
+
+test("reverseString", () => {
+  expect(reverseString("stefan")).toBe("nafets");
 });
